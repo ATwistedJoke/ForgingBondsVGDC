ec72b7f90b9cc9aae8163322b065feb204529587
Sri Mente <srimente@gmail.com>
2026-01-20 16:23:48 -0800

    Update SceneCommands.cs

--- Assets/Sri/Scripts and Dialogue Projects/SceneCommands.cs
+++ Assets/Sri/Scripts and Dialogue Projects/SceneCommands.cs
@@ -10,17 +10,15 @@ public class SceneCommands : MonoBehaviour
     public DialogueRunner dialogueRunner;
 
     public void Awake() {
-        
+
         DontDestroyOnLoad(dialogueRunner);
-        // Create a new command called 'camera_look', which looks at a target. 
-        // Note how we're listing 'GameObject' as the parameter type.
+        
         dialogueRunner.AddCommandHandler<string>(
             "load_scene",     // the name of the command
             LoadScene // the method to run
         );
     }
 
-    // The method that gets called when '<<camera_look>>' is run.
     private void LoadScene(string sceneName) 
     {
         SceneManager.LoadScene(sceneName);
