75bb1785ed85c962ab3509ae26153ccf73d6dfee
Sri Mente <srimente@gmail.com>
2026-01-24 14:42:22 -0800

    finished pre tutorial dialogue

--- Assets/Sri/Scripts and Dialogue Projects/GameManager.cs
+++ Assets/Sri/Scripts and Dialogue Projects/GameManager.cs
@@ -9,6 +9,8 @@ public class SceneCommands : MonoBehaviour
     // Drag and drop your Dialogue Runner into this variable.
     public DialogueRunner dialogueRunner;
 
+    int mentorAffinity = 0;
+
     public void Awake() {
 
         DontDestroyOnLoad(dialogueRunner);
@@ -17,11 +19,25 @@ public class SceneCommands : MonoBehaviour
             "load_scene",     // the name of the command
             LoadScene // the method to run
         );
+
+        dialogueRunner.AddCommandHandler<string, int>(
+            "change_affinity",
+            ChangeAffinity
+        );
     }
 
     private void LoadScene(string sceneName) 
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private void ChangeAffinity(string character, int modifier)
+    {
+        if(character == "mentor")
+        {
+            mentorAffinity += modifier;
+            Debug.Log("Affinity for "+ character + " changed to " + mentorAffinity);
+        }
     }
 
 
