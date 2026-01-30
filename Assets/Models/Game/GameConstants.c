d611558ff3479ee924bacb5e0d9664b57aa5516f
Annejulia Milian <156250774+monbebe0319@users.noreply.github.com>
2026-01-30 01:03:18 -0800

    Added GameState base and updated CharacterModel and font

---
+++ Assets/Models/Game/GameConstants.c
@@ -0,0 +1,21 @@
+public static class GameConstants
+{
+    // Attraction values
+    public const int ATTRACTION_MIN = 0;
+    public const int ATTRACTION_MAX = 100;
+    public const int ATTRACTION_INCREASE = 10;
+    public const int ATTRACTION_DECREASE = -5;
+
+    //Volume Values
+    public const int DEFAULT_VOLUME = 50;
+    public const int VOLUME_MAX = 100;
+    public const int VOLUME_MIN = 0;
+
+    //Screen size
+    public const int SCREEN_WIDTH = 500; //arbitrary val - replace later
+    public const int SCREEN_HEIGHT = 500; //arbitrary val
+
+    //Settings Overlay
+    public const int SETTINGS_WIDTH = 200; //arbitrary val
+    public const int SETTINGS_HEIGHT = 300; //arbitrary val
+}
--- ChacarcterControls/attractionConstants.c
+++
@@ -1,6 +0,0 @@
-public static class GameConstants
-{
-    // Attraction values
-    public const int ATTRACTION_MIN = 0;
-    public const int ATTRACTION_MAX = 100;
-}
