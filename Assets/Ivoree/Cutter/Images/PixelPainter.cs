25cbf341a6d0e2e7de1cc2ba16e3bea3b6c2cf59
Ivoree Sanchez <brs009@ucsd.edu>
2026-02-05 20:34:35 -0800

    12th Commit (Cutter)
    In this commit, I created the basic foundation for applying "paint" or "incising" certain tools. This is done by layering images and "removing" the first image slowly, like a lottery ticket. This should be able to be applied to any image and should be done by raking the same image and adding details, not changing the overall structure of the images.

---
+++ Assets/Ivoree/Cutter/Images/PixelPainter.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem; // Needed for mouse check
+using System.Collections.Generic;
+
+public class PixelPainter : MonoBehaviour
+{
+    [Header("Brush Settings")]
+    public GameObject paintBlobPrefab; 
+    public float pixelSpacing = 0.05f; // Keep this small for a smooth line
+
+    private Vector3 lastPaintPosition;
+
+    void Update()
+    {
+        // 1. Only paint if the Left Mouse Button is HELD DOWN
+        if (Mouse.current.leftButton.isPressed)
+        {
+            float dist = Vector3.Distance(transform.position, lastPaintPosition);
+
+            // 2. Spawn paint only if we moved enough
+            if (dist >= pixelSpacing)
+            {
+                ApplyPaint();
+            }
+        }
+        else
+        {
+            // Reset the "last position" when we let go so we can start a new line smoothly
+            lastPaintPosition = transform.position; 
+        }
+    }
+
+    void ApplyPaint()
+    {
+        // Snap to grid for clean pixel art look
+        float x = Mathf.Round(transform.position.x / pixelSpacing) * pixelSpacing;
+        float y = Mathf.Round(transform.position.y / pixelSpacing) * pixelSpacing;
+        Vector3 snapPos = new Vector3(x, y, 0);
+
+        Instantiate(paintBlobPrefab, snapPos, Quaternion.identity);
+        lastPaintPosition = transform.position;
+    }
+}
