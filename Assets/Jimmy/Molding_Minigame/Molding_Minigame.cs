9d6fa4a8f2c1bd09457ae3cbd00c207d67e14066
Jimmy Che <j2che@ucsd.edu>
2026-02-10 02:00:46 -0800

    spawning "works"

--- Assets/Jimmy/Molding_Minigame/Molding_Minigame.cs
+++ Assets/Jimmy/Molding_Minigame/Molding_Minigame.cs
@@ -2,26 +2,44 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem.LowLevel;
+using Unity.VisualScripting;
 
 public class Molding_Minigame : MonoBehaviour
 {
-    // public Stream stream;
-    // public RectTransform tipping_point;
-    public GameObject mold;
     public enum OreType { Iron, Gold, Mythril }
-    public Crucible crucible;
-    public int totalOreCount;
-    public GameObject game_container;
-    public RecipeGenerator recipeGenerator;
     public int total_molds = 0;
 
     public bool correct_mold = false;
+    public bool spawning = false;
+    public int totalOreCount;
+
+
+    public Mold mold;
+    public Crucible crucible;
+    public GameObject game_container;    
+    public RecipeGenerator recipeGenerator;
+    public Stream stream;
+
+    public float spawn_interval = 1.5f;
+
 
     private Mold current_mold;
     void Start_Minigame()
     {   
         game_container.SetActive(true);
+
+        if(mold != null)
+        {
+            SpawnNewMold();
+
+        }
         
+    }
+
+    void Start(){
+
+        Start_Minigame();
+
     }
 
     void End_Minigame()
@@ -55,27 +73,51 @@ public class Molding_Minigame : MonoBehaviour
 
         return true;
     }
+    
 
-    public void OnMoldFilled()
+    /* call when a mold is filled
+        clears the current contents in the crucible
+        check recipe using CheckRecipe()
+        delay of 0.5 seconds
+    */
+    public IEnumerator OnMoldFilled(Mold filledMold)
     {
+        // if (spawning)
+        // {
+        //     yield break;
+        // }
+
+        spawning = true; 
+
         Debug.Log("mold is filled");
-        crucible.ClearContents();
         if (CheckRecipe())
         {
             total_molds++;
         }
+        crucible.ClearContents();
+
+        yield return new WaitForSeconds (spawn_interval);
         SpawnNewMold();
+        Destroy(filledMold.gameObject);
+
+        // spawning = false;
     }
 
     public void SpawnNewMold()
     {
         Debug.Log("mold shold print");
-        RectTransform mold_rt = mold.GetComponent<RectTransform>();
 
-        GameObject new_mold = Instantiate(mold, mold_rt);
+        if(mold != null)
+        {
+        Mold new_mold = Instantiate(mold, game_container.transform);
+        Debug.Log("spawned at " + game_container.transform);
 
         current_mold = new_mold.GetComponent<Mold>();
 
         current_mold.game = this;
         }
+
+
+        }
+
 }
