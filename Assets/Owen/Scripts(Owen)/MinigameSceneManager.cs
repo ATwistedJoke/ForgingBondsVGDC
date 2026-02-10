b2d8bf0c7f0e98b11294d7dded361bf195dc99bc
Sri Mente <srimente@gmail.com>
2026-02-09 23:27:18 -0800

    test

--- Assets/Owen/Scripts(Owen)/MinigameSceneManager.cs
+++ Assets/Owen/Scripts(Owen)/MinigameSceneManager.cs
@@ -1,20 +1,23 @@
 using System;
+using System.Collections;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
 
 public class MinigameSceneManager : MonoBehaviour
 {
-    public DialogueRunner dialogueRunner; 
+    public DialogueRunner dr; 
     public GameObject minigamePrefab; 
+    public bool isLoading = false;
     public void Awake()
     {
-        DontDestroyOnLoad(dialogueRunner); 
-        dialogueRunner.AddCommandHandler<string>(
+        DontDestroyOnLoad(dr); 
+        dr.AddCommandHandler<string, string>(
             "load_scene",     // the name of the command
             LoadScene // the method to run
         );
-        dialogueRunner.AddCommandHandler<int>(
+        dr.AddCommandHandler<int>(
             "instance",
             InstantiatePrefab
         );
@@ -31,9 +34,24 @@ public class MinigameSceneManager : MonoBehaviour
         
     }
 
-    void LoadScene(string name)
+    private IEnumerator LoadScene(string name, string dialogueNode)
     {
+        yield return null;
+
+        isLoading = true;
+
+        dr.Stop();
+
         SceneManager.LoadScene(name); 
+
+        isLoading = false;
+
+        while(GameObject.FindGameObjectsWithTag("minigame").Length > 0 && isLoading == false)
+        {
+            yield return null;
+        }
+
+        dr.StartDialogue(dialogueNode);
     }
     void InstantiatePrefab(int idx)
     {
