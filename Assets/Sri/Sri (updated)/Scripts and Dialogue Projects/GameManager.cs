7d37213fb012c962a1328a229210b1d6fdc16b7c
ATwistedJoke <vroatis06@gmail.com>
2026-02-22 23:49:46 -0800

    I REMEMBER WE NEED MORE VFX OMG

--- Assets/Sri/Sri (updated)/Scripts and Dialogue Projects/GameManager.cs
+++ Assets/Sri/Sri (updated)/Scripts and Dialogue Projects/GameManager.cs
@@ -81,7 +81,7 @@ public class GameManager : MonoBehaviour
         dialogueRunner.AddCommandHandler("smelting_result", SmeltingMinigameResult);
         dialogueRunner.AddCommandHandler<bool>("MC",MCSpeak);
         dialogueRunner.AddCommandHandler<int>("Appearance", SetAppearance);
-        dialogueRunner.AddCommandHandler("SFX", PlayAudio);
+        dialogueRunner.AddCommandHandler<int>("SFX", PlayAudio);
         dialogueRunner.AddCommandHandler<int,int>("vc", VoiceLine);
         dialogueRunner.AddCommandHandler<float>("Theme", ChangeTheme);
         // dialogueRunner.AddCommandHandler<int>("change_corruption",ChangeCorruption);
@@ -332,9 +332,9 @@ public class GameManager : MonoBehaviour
     }
 
     //Audio Implementation
-    public void PlayAudio()
+    public void PlayAudio(int idx)
     {
-        AudioManager.instance.PlayOneShot(FMODEvents.instance.SFX[0], transform.position);
+        AudioManager.instance.PlayOneShot(FMODEvents.instance.SFX[idx], transform.position);
     }
 
     public void VoiceLine(int idx, int line)
